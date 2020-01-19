682dc666e52cd8039b2afa34c2e2fac4aa3fcdcd
Ilham Pratama <ilhamprt5@gmail.com>
2020-01-19 20:47:53 +0700

    Added some little changes

--- app/Project.js
+++ app/Project.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Style/main.css';
+
+const token = 'bearer 16d0044f47f49500f630fd5c99ba35a5deaa3118';
 
 const Project = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Welcome to my simple React-GraphQL App</h2>
+    </div>
+  );
 };
 
 export default Project;
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,13 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
