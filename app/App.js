909f5473024ae5a6495502984c4f494a5fbe14c8
Ilham Pratama <ilhamprt5@gmail.com>
2020-01-20 18:20:24 +0700

    Effectivity and Flexibility Increased

--- app/App.js
+++ app/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const token = 'bearer 16d0044f47f49500f630fd5c99ba35a5deaa3118';
-
 const Project = () => {
   return (
     <div>
--- app/index.js
+++ app/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Project from './Project';
+import Project from './App';
 import './Style/main.css';
+import axios from 'axios';
+
+const axiosGithubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`
+  }
+});
 
 ReactDom.render(<Project />, document.getElementById('root'));
